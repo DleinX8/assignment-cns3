3b939f1aecbca7199e2524c161ae73c7906a0578
DleinX8 <103602507+DleinX8@users.noreply.github.com>
2023-06-08 14:55:41 +0300

    Add files via upload

---
+++ MyWeather/MyWeather/app/src/main/java/com/example/MyWeather/ui/theme/Type.kt
@@ -0,0 +1,27 @@
+package com.example.MyWeather.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.MyWeather.R
+
+
+val CustomFont = FontFamily(
+    Font(R.font.futura_normal),
+    Font(R.font.futura_bold, FontWeight.Bold),
+    Font(R.font.futura_medium, FontWeight.Normal)
+
+)
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = CustomFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        // FontFamily=CustomFont
+    )
